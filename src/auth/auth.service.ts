9afd7aea72a3e830960de815f55b799713dd0ad8
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-10-09 14:37:46 -0400

    Creando usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,20 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { SignupInput } from './dto/inputs/signup.input';
 import { AuthResponse } from './types/auth-response.types';
 import { User } from 'src/users/entities/user.entity';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
 
-    constructor() {}
+    constructor(
+        private readonly usersService: UsersService,
+    ) {}
 
     async signup(signupInput: SignupInput): Promise<AuthResponse> { 
 
-        console.log({signupInput})
-        // return {
-        //     token: 'adasd',
-        //     user: new User()
-        // }
+        //Todo: crear usuario
+        const user = await this.usersService.create( signupInput )
 
-        throw new Error('No implementado')        
+        //Todo: crear JWT
+        const token = 'ABC123';
+
+        return {token, user}
+               
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
-import { User } from './entities/user.entity';
+
+import { SignupInput } from '../auth/dto/inputs/signup.input';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+  
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>
+  ){}
+
+  async create( signupInput: SignupInput ): Promise<User> {
+    try {
+
+      const newUser = this.usersRepository.create( signupInput )
+
+      return await this.usersRepository.save( newUser ) 
+
+    } catch (error) {
+      console.log(error)
+      throw new BadRequestException('Algo salió mal')
+    }
   }
 
   async findAll(): Promise<User[]> {
