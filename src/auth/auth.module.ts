ae9abc19705848e406bcc7937e2b9bccdb18736f
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-10-09 22:07:27 -0400

    JWT Strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { PassportModule } from '@nestjs/passport';
 import { Module } from '@nestjs/common';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [ AuthResolver, AuthService ],
+  providers: [ AuthResolver, AuthService, JwtStrategy],
+  exports: [ JwtStrategy, PassportModule, JwtModule ],
   imports: [
 
     ConfigModule,
